260933b6e1f4f5459b130a96c39d1e944b120c46
lunjingjie <lunjj@create80.com>
2022-08-09 16:58:14 +0800

    feat: init

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1658980513952_5209',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
---
+++ src/service/alarm-info/alarmInfo.service.ts
@@ -0,0 +1,5 @@
+export class AlarmInfoService {
+  getAlarmList() {
+    return [];
+  }
+}
